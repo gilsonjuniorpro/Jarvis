d10bf41639a759c92460cb59a80e2138dc49187f
gilsonjuniorpro <gilsonjuniorpro@gmail.com>
2020-01-29 13:02:20 -0500

    create methods with context as parameter

--- jarvis/src/main/java/com/jarvis/ca/Mark.kt
+++ jarvis/src/main/java/com/jarvis/ca/Mark.kt
@@ -1,6 +1,7 @@
 package com.jarvis.ca
 
 import android.app.Activity
+import android.content.Context
 import android.view.View
 import android.widget.ImageView
 import android.widget.TextView
@@ -27,6 +28,24 @@ class Mark {
                 .show()
         }
 
+        fun showAlertSuccess(context: Context, title: String, message: String){
+            Alerter.create(context as Activity, R.layout.layout_with_title)
+                .setBackgroundColorRes(R.color.colorAlertSuccess)
+                .enableSwipeToDismiss()
+                .setDuration(3000)
+                .also { alerter ->
+                    val layout = alerter.getLayoutContainer()
+                    var ivIcon: ImageView = layout?.findViewById<View>(R.id.ivIcon) as ImageView
+                    var tvTitle: TextView = layout?.findViewById<View>(R.id.tvTitle) as TextView
+                    var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
+
+                    ivIcon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_success))
+                    tvTitle.text = title
+                    tvMessage.text = message
+                }
+                .show()
+        }
+
         fun showAlertSuccess(activity: Activity, message: String){
             Alerter.create(activity, R.layout.layout_no_title)
                 .setBackgroundColorRes(R.color.colorAlertSuccess)
@@ -38,6 +57,22 @@ class Mark {
                     var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
 
                     ivIcon.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_success))
+                    tvMessage.text = message
+                }
+                .show()
+        }
+
+        fun showAlertSuccess(context: Context, message: String){
+            Alerter.create(context as Activity, R.layout.layout_no_title)
+                .setBackgroundColorRes(R.color.colorAlertSuccess)
+                .enableSwipeToDismiss()
+                .setDuration(3000)
+                .also { alerter ->
+                    val layout = alerter.getLayoutContainer()
+                    var ivIcon: ImageView = layout?.findViewById<View>(R.id.ivIcon) as ImageView
+                    var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
+
+                    ivIcon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_success))
                     tvMessage.text = message
                 }
                 .show()
@@ -61,6 +96,24 @@ class Mark {
                 .show()
         }
 
+        fun showAlertError(context: Context, title: String, message: String){
+            Alerter.create(context as Activity, R.layout.layout_with_title)
+                .setBackgroundColorRes(R.color.colorAlertError)
+                .enableSwipeToDismiss()
+                .setDuration(3000)
+                .also { alerter ->
+                    val layout = alerter.getLayoutContainer()
+                    var ivIcon: ImageView = layout?.findViewById<View>(R.id.ivIcon) as ImageView
+                    var tvTitle: TextView = layout?.findViewById<View>(R.id.tvTitle) as TextView
+                    var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
+
+                    ivIcon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_error))
+                    tvTitle.text = title
+                    tvMessage.text = message
+                }
+                .show()
+        }
+
         fun showAlertError(activity: Activity, message: String){
             Alerter.create(activity, R.layout.layout_no_title)
                 .setBackgroundColorRes(R.color.colorAlertError)
@@ -72,6 +125,22 @@ class Mark {
                     var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
 
                     ivIcon.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_error))
+                    tvMessage.text = message
+                }
+                .show()
+        }
+
+        fun showAlertError(context: Context, message: String){
+            Alerter.create(context as Activity, R.layout.layout_no_title)
+                .setBackgroundColorRes(R.color.colorAlertError)
+                .enableSwipeToDismiss()
+                .setDuration(3000)
+                .also { alerter ->
+                    val layout = alerter.getLayoutContainer()
+                    var ivIcon: ImageView = layout?.findViewById<View>(R.id.ivIcon) as ImageView
+                    var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
+
+                    ivIcon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_error))
                     tvMessage.text = message
                 }
                 .show()
@@ -95,6 +164,24 @@ class Mark {
                 .show()
         }
 
+        fun showAlertWarning(context: Context, title: String?, message: String){
+            Alerter.create(context as Activity, R.layout.layout_with_title)
+                .setBackgroundColorRes(R.color.colorAlertWarning)
+                .enableSwipeToDismiss()
+                .setDuration(3000)
+                .also { alerter ->
+                    val layout = alerter.getLayoutContainer()
+                    var ivIcon: ImageView = layout?.findViewById<View>(R.id.ivIcon) as ImageView
+                    var tvTitle: TextView = layout?.findViewById<View>(R.id.tvTitle) as TextView
+                    var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
+
+                    ivIcon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_info))
+                    tvTitle.text = title
+                    tvMessage.text = message
+                }
+                .show()
+        }
+
         fun showAlertWarning(activity: Activity, message: String){
             Alerter.create(activity, R.layout.layout_no_title)
                 .setBackgroundColorRes(R.color.colorAlertWarning)
@@ -107,6 +194,23 @@ class Mark {
                     var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
 
                     ivIcon.setImageDrawable(ContextCompat.getDrawable(activity, R.drawable.ic_info))
+                    tvMessage.text = message
+                }
+                .show()
+        }
+
+        fun showAlertWarning(context: Context, message: String){
+            Alerter.create(context as Activity, R.layout.layout_no_title)
+                .setBackgroundColorRes(R.color.colorAlertWarning)
+                .enableSwipeToDismiss()
+                .setDuration(3000)
+                .also { alerter ->
+                    val layout = alerter.getLayoutContainer()
+                    var ivIcon: ImageView = layout?.findViewById<View>(R.id.ivIcon) as ImageView
+                    var tvTitle: TextView = layout?.findViewById<View>(R.id.tvTitle) as TextView
+                    var tvMessage: TextView = layout?.findViewById<View>(R.id.tvMessage) as TextView
+
+                    ivIcon.setImageDrawable(ContextCompat.getDrawable(context, R.drawable.ic_info))
                     tvMessage.text = message
                 }
                 .show()
